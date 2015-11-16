afd4c747486d7cf1bd4f5c88de8d6b292006e99f
ERRECabrera <juanparanadie@gmail.com>
2015-11-16 14:51:04 +0100

    Seeds to Timetracking App - Day16

--- week4/day1/timetracking/app/models/project.rb
+++ week4/day1/timetracking/app/models/project.rb
@@ -1,5 +1,11 @@
 class Project < ActiveRecord::Base
+
 	def self.iron_find(params)
-			where(params).first
+		where(params).first
 	end
+
+	def self.clean_old
+		where("update_at < ?", Date.today - 1.week).destroy_all
+	end
+
 end
--- week4/day1/timetracking/db/seeds.rb
+++ week4/day1/timetracking/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..25).each do |num|
+	Project.create name: "Project #{num}"
+end
