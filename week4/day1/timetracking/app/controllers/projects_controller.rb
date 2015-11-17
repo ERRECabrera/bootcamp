c6a5afb456d8a6a7e990cf4cb762d7156f8d2474
ERRECabrera <juanparanadie@gmail.com>
2015-11-17 12:49:26 +0100

    TaskTables in Timetracking App - Day17

--- week4/day1/timetracking/app/controllers/projects_controller.rb
+++ week4/day1/timetracking/app/controllers/projects_controller.rb
@@ -1,7 +1,19 @@
 class ProjectsController < ApplicationController
 	def index
 		#@projects = Project.last_created_project(10)
-    @projects = Project.first_updated(10)
-		render "index"
+    @projects = Project.last_created_project(10)
+    if @projects.empty?
+      render template: "others/no_projects"
+    else
+		  render "index"
+    end
 	end
+  def show
+    unless @project = Project.find_by(id: params[:id])
+      @project_id = params[:id]
+      render "no_project_found"
+    else
+    render "show"
+    end
+  end
 end
--- week4/day1/timetracking/db/seeds.rb
+++ week4/day1/timetracking/db/seeds.rb
@@ -7,5 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 (1..25).each do |num|
-	Project.create name: "Project #{num}"
+	Project.create name: "Project #{num}", description: "a"*num
+end
+
+(1..50).each do |num|
+  Entry.create(project_id: rand(1..25), hours: rand(0..3), minutes: rand(00..59), comments: "larala"*rand(0..num), date: Time.now-rand(1..num).hours)
 end
