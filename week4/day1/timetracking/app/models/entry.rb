246bce792310693706549c47517e48f4345d0210
ERRECabrera <juanparanadie@gmail.com>
2015-11-18 12:07:30 +0100

    continue Process&ShowDatas in Timetracking App - Day18

--- week4/day1/timetracking/app/models/entry.rb
+++ week4/day1/timetracking/app/models/entry.rb
@@ -1,4 +1,9 @@
 class Entry < ActiveRecord::Base
   belongs_to :project
 
+  validates :hours, :minutes, numericality: {only_integer: true}
+  validates :hours, :minutes, :date, presence: true
+  validates :minutes, length {maximum: 59}
+  validates :project_id, presence: true
+
 end
--- week4/day1/timetracking/app/models/project.rb
+++ week4/day1/timetracking/app/models/project.rb
@@ -1,6 +1,11 @@
 class Project < ActiveRecord::Base
   has_many :entries
 
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :name, length: {maximum: 30}
+  validates :name, format: {with: /\A[\wÑñ\ ]+\z/}
+
 	def self.iron_find(params)
 		where(params).first
 	end
